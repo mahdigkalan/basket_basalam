39c1060d448a098089d611be3ed8aa662b052f70
MAHDI <mahdigholizadek@gmail.com>
2021-03-06 11:48:58 +0330

    js

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $(".add-button").click(function () {
-        var productNumber =  $(this).siblings("input").val() ;
-        productNumber++ ;
-        $(this).siblings("input").val(productNumber) ;
-    });
+//     $(".add-button").click(function () {
+//         var productNumber =  $(this).siblings("input").val() ;
+//         productNumber++ ;
+//         $(this).siblings("input").val(productNumber) ;
+//     });
 
-    $(".less-button").click(function () {
+//     $(".less-button").click(function () {
 
-        var productNumber =  $(this).siblings("input").val() ;
-        if(productNumber > 1){
-            productNumber = parseInt(productNumber-1)
-        $(this).siblings("input").val(productNumber) ;
-        }
+//         var productNumber =  $(this).siblings("input").val() ;
+//         if(productNumber > 1){
+//             productNumber = parseInt(productNumber-1)
+//         $(this).siblings("input").val(productNumber) ;
+//         }
 
-    });
+//     });
 
-    $(".delButton").click(function (e) {
-        console.log(e.target.nodeName);
-        var target = $(e.target).parent().parent().parent().parent().parent() ;
-        var height = $(target).css("height") ;
-        var product = $(this).parents(".product") ;
-        height = height.replace("px", "");
-        $(product).css("display","none") ;
-        height = parseInt(height-180) ;
-        $(target).css("height" , +height+"px") ;
-        console.log(height);
-        if(height <= 240){
-            $(target).css("display","none") ;
-        }
-        $(product).removeClass("product") ;
-        var pro = $(".product").length ;
-        console.log(pro);
-        $(".small-badge").html(pro);
-    });
+//     $(".delButton").click(function (e) {
+//         console.log(e.target.nodeName);
+//         var target = $(e.target).parent().parent().parent().parent().parent() ;
+//         var height = $(target).css("height") ;
+//         var product = $(this).parents(".product") ;
+//         height = height.replace("px", "");
+//         $(product).css("display","none") ;
+//         height = parseInt(height-180) ;
+//         $(target).css("height" , +height+"px") ;
+//         console.log(height);
+//         if(height <= 240){
+//             $(target).css("display","none") ;
+//         }
+//         $(product).removeClass("product") ;
+//         var pro = $(".product").length ;
+//         console.log(pro);
+//         $(".small-badge").html(pro);
+//     });
 
-});
+// });
+
+
+document.querySelectorAll(".add-button").addEventListener("click",function(){
+    console.log(tis);
+})
+
